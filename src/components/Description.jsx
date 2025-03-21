d2aed042a323ba4c4d3487a45c75f85b3ad30b49
Venus <venusyip@outlook.com>
2025-03-21 17:46:44 +0000

    Add markup for result section

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,1 +1,10 @@
-export default function Description() {}
+export default function Description() {
+  return (
+    <section className='description'>
+      <h3 className='description__heading'>Great</h3>
+      <p className='description__body'>
+        You scored higher than 65% of the people who have taken these tests
+      </p>
+    </section>
+  );
+}
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,1 +1,8 @@
-export default function Score() {}
+export default function Score() {
+  return (
+    <div className='result-section__score'>
+      <p className='result-section__value'>76</p>
+      <p className='result-section__divisor'>of 100</p>
+    </div>
+  );
+}
