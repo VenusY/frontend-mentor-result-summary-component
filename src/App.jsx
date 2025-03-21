8a888f0e1e283e40054a6858024f0c7c35647f72
Venus <venusyip@outlook.com>
2025-03-21 02:16:19 +0000

    Add ResultSection and SummarSection components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
-export default function App() {}
+import ResultSection from './components/ResultSection.jsx';
+import SummarySection from './components/SummarySection.jsx';
+
+export default function App() {
+  return (
+    <div className='card'>
+      <ResultSection />
+      <SummarySection />
+    </div>
+  );
+}
