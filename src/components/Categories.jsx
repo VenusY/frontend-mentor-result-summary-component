bd2844e394b03de163f5c097f75792d1f26a3b1c
Venus <venusyip@outlook.com>
2025-03-22 05:08:08 +0000

    Fix elements not appearing

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,15 +4,17 @@ export default function Categories() {
   return (
     <>
       {data.map((category) => {
-        <div className='category'>
-          <img
-            src={require(`../assets/images/icon-${category.name.toLowerCase()}.svg`)}
-            alt={`${category.name} icon`}
-            className='category__icon'
-          />
-          <p className='category__name'>{category.name}</p>
-          <p className='category__score'>{category.score}</p>
-        </div>;
+        return (
+          <div className='category'>
+            <img
+              src={require(`../assets/images/icon-${category.name.toLowerCase()}.svg`)}
+              alt={`${category.name} icon`}
+              className='category__icon'
+            />
+            <p className='category__name'>{category.name}</p>
+            <p className='category__score'>{category.score}</p>
+          </div>
+        );
       })}
     </>
   );
