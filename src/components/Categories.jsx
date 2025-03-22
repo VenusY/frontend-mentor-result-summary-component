fc1121f2730109b3e957e4381ea7a384b0a46e67
Venus <venusyip@outlook.com>
2025-03-22 04:29:22 +0000

    Add markup to Categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,1 +1,19 @@
-export default function Categories() {}
+import data from 'data.json';
+
+export default function Categories() {
+  return (
+    <>
+      {data.map((category) => {
+        <div className='category'>
+          <img
+            src={require(`../images/icon-${category.name.toLowerCase()}.svg`)}
+            alt={`${category.name} icon`}
+            className='category__icon'
+          />
+          <p className='category__name'>{category.name}</p>
+          <p className='category__score'>{category.score}</p>
+        </div>;
+      })}
+    </>
+  );
+}
