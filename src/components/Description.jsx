4892563abcda588c86a85de6b2e3cd20d36bc85a
Venus <venusyip@outlook.com>
2025-03-22 21:56:51 +0000

    Add missing punctuation to description body

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -3,7 +3,7 @@ export default function Description() {
     <section className='description'>
       <h3 className='description__heading'>Great</h3>
       <p className='description__body'>
-        You scored higher than 65% of the people who have taken these tests
+        You scored higher than 65% of the people who have taken these tests.
       </p>
     </section>
   );
