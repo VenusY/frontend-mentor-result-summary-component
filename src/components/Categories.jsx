202a916dfff5d4a44a2ba164d2ab71ab8bb14373
Venus <venusyip@outlook.com>
2025-03-23 03:44:21 +0000

    Add classes and span tag

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,20 +2,26 @@ import data from '../data.json';
 
 export default function Categories() {
   return (
-    <>
+    <div className='category__container'>
       {data.map((category) => {
         return (
-          <div key={category.name} className='category'>
+          <div
+            key={category.name}
+            className={`category category--${category.name.toLowerCase()}`}
+          >
             <img
               src={require(`../assets/images/icon-${category.name.toLowerCase()}.svg`)}
               alt={`${category.name} icon`}
               className='category__icon'
             />
             <p className='category__name'>{category.name}</p>
-            <p className='category__score'>{category.score}</p>
+            <p className='category__score'>
+              <span className='category__score--bold'>{category.score}</span> /
+              100
+            </p>
           </div>
         );
       })}
-    </>
+    </div>
   );
 }
