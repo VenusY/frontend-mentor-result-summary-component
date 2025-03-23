8501299f40c4eabe1731733e463a08f07f3eb1cd
Venus <venusyip@outlook.com>
2025-03-23 17:17:13 +0000

    Change asset/resource config options

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -29,7 +29,7 @@ module.exports = {
         test: /\.(png|jpe?g|svg)$/i,
         type: 'asset/resource',
         generator: {
-          filename: '[hash][ext]',
+          filename: 'assets/[name][ext]',
         },
       },
     ],
